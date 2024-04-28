007fc69eaa90e7f810184abfa883ed943b3d28e5
Kirill T <sknxq1@gmail.com>
2024-04-28 23:30:09 +0300

    add reply handler

--- handlers/text_handlers.py
+++ handlers/text_handlers.py
@@ -1,8 +1,10 @@
 from aiogram import Router, F
-from aiogram.types import Message, ReactionTypeEmoji
+from aiogram.types import Message, ReactionTypeEmoji, ReplyKeyboardRemove
+from asyncio import sleep
 
 from lexicon import LEXICON_RU
-from keyboards import keyboard_choice
+from keyboards import keyboard_choice, keyboard_start
+from tools import randomize_choice, get_winner
 
 
 router = Router()
@@ -10,9 +12,9 @@ router.message.filter(F.text)
 
 
 choices = [
-    LEXICON_RU.button_rock,
-    LEXICON_RU.button_paper,
-    LEXICON_RU.button_scissors
+    LEXICON_RU.button_rock.value,
+    LEXICON_RU.button_paper.value,
+    LEXICON_RU.button_scissors.value,
 ]
 
 
@@ -27,11 +29,23 @@ async def process_agreement(message: Message):
 @router.message(F.text == LEXICON_RU.button_no.value)
 async def process_rejection(message: Message):
     await message.answer(
-        text=LEXICON_RU.reply_to_rejection.value
+        text=LEXICON_RU.reply_to_rejection.value,
+        reply_markup=ReplyKeyboardRemove(),
     )
 
 
-@router.message(F.text.in_([]))
+@router.message(F.text.in_(choices))
+async def process_game_reply(message: Message):
+    await message.answer(
+        text=randomize_choice(),
+        reply_markup=ReplyKeyboardRemove(),
+        )
+    await sleep(0.3)
+    await message.answer(text=get_winner())
+    await message.answer(
+        text=LEXICON_RU.ask_new_game.value,
+        reply_markup=keyboard_start,
+        )
 
 
 @router.message()
