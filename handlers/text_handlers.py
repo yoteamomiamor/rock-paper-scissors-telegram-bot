b039fef070a597e9346c727b0fae9352e932e3cc
Kirill T <sknxq1@gmail.com>
2024-04-28 22:45:48 +0300

    update text handlers

--- handlers/text_handlers.py
+++ handlers/text_handlers.py
@@ -1,5 +1,5 @@
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, ReactionTypeEmoji
 
 from lexicon import LEXICON_RU
 from keyboards import keyboard_choice
@@ -7,6 +7,13 @@ from keyboards import keyboard_choice
 
 router = Router()
 router.message.filter(F.text)
+
+
+choices = [
+    LEXICON_RU.button_rock,
+    LEXICON_RU.button_paper,
+    LEXICON_RU.button_scissors
+]
 
 
 @router.message(F.text == LEXICON_RU.button_yes.value)
@@ -22,3 +29,13 @@ async def process_rejection(message: Message):
     await message.answer(
         text=LEXICON_RU.reply_to_rejection.value
     )
+
+
+@router.message(F.text.in_([]))
+
+
+@router.message()
+async def process_empty_message(message: Message):
+    await message.reply(text=LEXICON_RU.no_reply.value)
+    await message.react(
+        reaction=[ReactionTypeEmoji(emoji=LEXICON_RU.reaction.value)])
