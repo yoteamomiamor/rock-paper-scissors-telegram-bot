6d87c3ee28107c3543e12af20c94d97fac118482
Kirill T <sknxq1@gmail.com>
2024-04-28 18:43:25 +0300

    update keyboards

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,12 +1,11 @@
-from aiogram.types import (KeyboardButton, ReplyKeyboardMarkup,
-                           ReplyKeyboardRemove)
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from lexicon import LEXICON_RU
 
 
 keyboard_start = ReplyKeyboardMarkup(
     keyboard=[[
-        KeyboardButton(text=LEXICON_RU.button_yes),
-        KeyboardButton(text=LEXICON_RU.button_no),
+        KeyboardButton(text=LEXICON_RU.button_yes.value),
+        KeyboardButton(text=LEXICON_RU.button_no.value),
         ]],
     resize_keyboard=True,
     )
@@ -14,9 +13,9 @@ keyboard_start = ReplyKeyboardMarkup(
 
 keyboard_choice = ReplyKeyboardMarkup(
     keyboard=[[
-        KeyboardButton(text=LEXICON_RU.button_rock),
-        KeyboardButton(text=LEXICON_RU.button_paper),
-        KeyboardButton(text=LEXICON_RU.button_scissors),
+        KeyboardButton(text=LEXICON_RU.Choice.button_rock.value),
+        KeyboardButton(text=LEXICON_RU.Choice.button_paper.value),
+        KeyboardButton(text=LEXICON_RU.Choice.button_scissors.value),
     ]],
     resize_keyboard=True,
 )
