a023910b37c9200268eae696b7a7ee49c60cfe85
Kirill T <sknxq1@gmail.com>
2024-04-28 15:16:08 +0300

    add new lexicon

--- lexicon/lexincon.py
+++ lexicon/lexincon.py
@@ -6,5 +6,10 @@ class LEXICON_RU(Enum):
     command_help: str = ('in this bot you can play rock-paper-scissors with me ^^ '
                          'you and i choose one of the items (rock-paper-scissors) '
                          'and someone wins according to the rules!!')
+
     button_yes: str = 'sure!!'
     button_no: str = 'not now'
+
+    button_rock: str = '🗿 rock'
+    button_paper: str = '📜 paper'
+    button_scissors: str = '✂️ scissors'
