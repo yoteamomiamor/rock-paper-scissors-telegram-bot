1b2e91881330b5bef02082d597c9b5127dc656bc
Kirill T <sknxq1@gmail.com>
2024-04-28 21:18:36 +0300

    refactoring

--- tools/tools.py
+++ tools/tools.py
@@ -1,10 +1,9 @@
 from random import choice
-from typing import Literal
 from lexicon import LEXICON_RU
 
 
 def randomize_choice() -> str:
-    return choice((LEXICON_RU.Choice.button_rock,
-                   LEXICON_RU.Choice.button_paper,
-                   LEXICON_RU.Choice.button_scissors,
+    return choice((LEXICON_RU.button_rock,
+                   LEXICON_RU.button_paper,
+                   LEXICON_RU.button_scissors,
                    ))
