ac4c555e2cfc176f4995c0de1e63ee5b0d96dc02
Kirill T <sknxq1@gmail.com>
2024-04-29 13:26:38 +0300

    add algorith that returns the winner

--- tools/tools.py
+++ tools/tools.py
@@ -8,5 +8,17 @@ def randomize_choice() -> str:
                    LEXICON_RU.button_scissors.value,
                    ))
 
-def get_winner() -> str:
-    return LEXICON_RU.reply_to_draw.value
+def get_winner(user_choice: str, bot_choice: str) -> str:
+    choice_value = {
+        LEXICON_RU.button_rock.value: 0,
+        LEXICON_RU.button_paper.value: 1,
+        LEXICON_RU.button_scissors.value: 2
+    }
+
+    result = (
+        LEXICON_RU.reply_to_draw.value,
+        LEXICON_RU.reply_to_victory.value,
+        LEXICON_RU.reply_to_loss.value,
+    )
+
+    return result[choice_value[user_choice] - choice_value[bot_choice]]
